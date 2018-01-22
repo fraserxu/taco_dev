92d386f492f12f9f0559d6874967867f4228021d
Fraser Xu <xvfeng123@gmail.com>
2018-01-22 16:41:59 +1100

    create utils

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,16 @@
 extern crate clap;
-extern crate glob;
+extern crate taco_dev;
 
 use std::error::Error;
 use std::io::prelude::*;
 use std::fs::File;
 use std::path::Path;
-use std::process::Command;
 use clap::{App, Arg, SubCommand};
-use glob::glob;
 
-static DNSMASQ_DEFAULT_CONFIG: &'static str = 
+use taco_dev::utils::{run_command, copy_files};
+
+static DNSMASQ_DEFAULT_CONFIG: &'static str =
 "address=/.test/127.0.0.1";
-
-fn copy_files(from: &str, to: &str) {
-    for entry in glob(from).expect("Failed to read glob pattern") {
-        match entry {
-            Ok(path) => {
-                let file_os_string = path.into_os_string();
-                let file = file_os_string.to_str().unwrap();
-                run_command("cp", vec![file, to]);
-            },
-            Err(e) => println!("{:?}", e),
-        }
-    }
-}
-
-fn run_command(cmd: &str, args: Vec<&str>) {
-    let command = Command::new(cmd)
-        .args(args)
-        .output()
-        .expect("failed to execute process");
-
-    if command.status.success() {
-        println!("{} succeeded", cmd);
-    } else {
-        let s = String::from_utf8_lossy(&command.stderr);
-        println!("{} failed, stderr: {}", cmd, s);
-    }
-}
 
 fn add_dnsmasq_to_launchctl() {
     copy_files("/usr/local/opt/dnsmasq/*.plist", "/Library/LaunchDaemons");
@@ -74,7 +47,7 @@ fn main() {
 
         let path = Path::new("/usr/local/etc/dnsmasq.conf2");
         let display = path.display();
-        
+
         let mut file = match File::create(&path) {
             Err(why) => panic!("couldn't create {}: {}", display, why.description()),
             Ok(file) => file,
@@ -98,4 +71,4 @@ fn main() {
         let upstream_server = matches.value_of("upstream").unwrap_or("127.0.0.1:8000");
         println!("Upstream server: {}", upstream_server);
     }
-} 
+}
---
+++ src/utils.rs
@@ -0,0 +1,29 @@
+use std::process::Command;
+use glob::glob;
+
+pub fn copy_files(from: &str, to: &str) {
+    for entry in glob(from).expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => {
+                let file_os_string = path.into_os_string();
+                let file = file_os_string.to_str().unwrap();
+                run_command("cp", vec![file, to]);
+            },
+            Err(e) => println!("{:?}", e),
+        }
+    }
+}
+
+pub fn run_command(cmd: &str, args: Vec<&str>) {
+    let command = Command::new(cmd)
+        .args(args)
+        .output()
+        .expect("failed to execute process");
+
+    if command.status.success() {
+        println!("{} succeeded", cmd);
+    } else {
+        let s = String::from_utf8_lossy(&command.stderr);
+        println!("{} failed, stderr: {}", cmd, s);
+    }
+}
